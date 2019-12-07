dd51142b1e67c3e9c95d7312ef4a05f6300e3994
Acs, Geza (External) <geza.acs.ext@evosoft.com>
2019-12-07 23:23:09 +0100

    aoc_6

--- src/aoc_6/Day.kt
+++ src/aoc_6/Day.kt
@@ -1,37 +1,21 @@
 package aoc_6
 import RunAoc
 import java.io.File
-import java.util.*
-
 class Day: RunAoc {
-    val planets = mutableMapOf<String,Planet>().apply { this.put("COM",Planet("COM","0")) }
-
-    class Planet(val name:String,val previous:String) {
-        var orbits=0
-        override fun toString(): String {
-            return orbits.toString()
-        }
+    val lines= File("aoc_6/input.txt")
+        .readLines()
+        .map { it.split(")") }
+        .associate { pairs -> pairs[1] to pairs[0]  }
+    fun setOfPlanet(name:String) : MutableSet<String> {
+        return if (name=="COM") mutableSetOf("COM") else setOfPlanet(lines[name]!!).apply { add(name) }
     }
+    override fun a(): Int = lines.keys.sumBy { setOfPlanet(it).size - 1 }
 
-    override fun a(): Int {
-        val lines= File("aoc_6/input_s.txt")
-                .readLines()
-                .map { it.split(")") }
-                .associate { pairs -> pairs[1] to pairs[0]  }
-
-        fun setOfPlanet(name:String) : MutableSet<String> {
-            return if (name=="COM") mutableSetOf("COM") else setOfPlanet(lines[name]!!).apply { add(name) }
-        }
-
-        return lines.keys.sumBy { setOfPlanet(it).size - 1 }
-    }
-
-    override fun b():Int {
-        return 0
-    }
+    override fun b():Int= (setOfPlanet("YOU")-setOfPlanet("SAN")).size+(setOfPlanet("SAN")-setOfPlanet("YOU")).size-2
 }
 
 fun main() {
     val day=Day()
-    println(day.a())
+    println(day.b())
+
 }
