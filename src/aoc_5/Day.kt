bdfcd6e53912342f3ad19090dfa768dd5c2290fa
Acs, Geza (External) <geza.acs.ext@evosoft.com>
2019-12-07 04:40:55 +0100

    aoc_5 part b

--- src/aoc_5/Day.kt
+++ src/aoc_5/Day.kt
@@ -7,7 +7,7 @@ import java.util.*
 class Day: RunAoc {
 
     val init_memory:MutableList<Int> by lazy {resetMemory()}
-    val INPUT_VALUE=1
+    val INPUT_VALUE=5
     lateinit var ls:MutableList<Int>
     var pointer=0
     var control = ""
@@ -44,12 +44,25 @@ class Day: RunAoc {
                 2 -> write(read()*read())
                 3 -> input(INPUT_VALUE)
                 4 -> println(read())
+                5 -> jumpTrue()
+                6 -> jumpFalse()
+                7 -> lessThan()
+                8 -> equalPars()
                 99 -> break@loop
             }
 //            println("Pointer:"+pointer+"  Value:"+ls[pointer])
         }
         return ls[0]
     }
+
+    private fun equalPars() = write(if(read()==read())  1 else 0)
+
+    private fun lessThan() = write(if(read()<read())  1 else 0)
+
+
+    private fun jumpTrue()  { if(read()!=0) pointer=read() else pointer++ }
+    private fun jumpFalse() { if(read()==0) pointer=read() else pointer++ }
+
 
     fun controllast(x:Int=1):Int {
         val maxchar=if(x>control.length) control.length else x
@@ -80,6 +93,8 @@ fun main() {
     val day=Day()
 //    return
 //    day.testList(mutableListOf<Int>(3,2,4,0,99))
-    day.testList(mutableListOf<Int>(1,1,1,4,99,5,6,0,99))
+    day.testList(mutableListOf<Int>(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99))
     println(day.instruct())
 }
